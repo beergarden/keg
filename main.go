f24a0cf61807b6d7cb3d2c0167d4f4893ae07736
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2015-06-01 23:45:50 +0900

    Hello world with LED.

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/stianeikeland/go-rpio"
+	"os"
+	"time"
+)
+
+var (
+	// mcu pin 10 corresponds to physical pin 19.
+	pin = rpio.Pin(10)
+)
 
 func main() {
-	fmt.Println("Hello, World!\n")
+	err := rpio.Open()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer rpio.Close()
+
+	pin.Output()
+
+	for x := 0; x < 20; x++ {
+		fmt.Print(".")
+		pin.Toggle()
+		time.Sleep(time.Second / 5)
+	}
+
+	fmt.Println("")
+	fmt.Println("Done")
 }
